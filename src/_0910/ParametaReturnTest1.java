ca5a71e4a9980293a724b957f381ec2264c87abb
dangdang122 <vmfhxhtm122@naver.com>
2025-09-10 09:25:48 +0900

    계산기

---
+++ src/_0910/ParametaReturnTest1.java
@@ -0,0 +1,17 @@
+package _0910;
+
+import java.util.Scanner;
+
+public class ParametaReturnTest1 {
+    public static void main(String[] args) {
+        System.out.println("2개의 정수의 합");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("* 정수1 입력: ");
+        int num1 = sc.nextInt();
+        System.out.print("* 정수2 입력: ");
+        int num2 = sc.nextInt();
+        Calc1 calc1 = new Calc1();
+        int result = calc1.plus(num1,num2);
+        System.out.println("덧셈결과 =" +result);
+    }
+}
